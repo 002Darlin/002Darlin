93d411e833616e2109c1f698f09a43903f89eda5
02Darling <wzs02darling@163.com>
2022-09-24 23:53:31 +0800

    JavaProgrammingStart

---
+++ JavaProgrammingStart/src/HelloWorld.java
@@ -0,0 +1,30 @@
+/*Java语言
+ 面向对象的语言
+ 健壮性和安全性
+ 有良好的跨平台特性
+ 支持多线程
+  */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        var info= """
+                  好好学习，
+                  天天向上
+                  """;
+        System.out.println(info);
+    }
+}
+/*
+创建一个Java项目
+指定JDK 项目名 包名(网址反写)
+创建Java类 类名通常是名词 大写字母开头
+设置项目JDK版本
+Project Structure - SDKs Documentation Paths URL
+https://docs.oracle.com/en/java/javase/15/docs/api
+Ctrl+Q
+源码查看 Ctrl+左键
+命令行 View-Tool Windows-Terminal
+ */
+/*
+配置IntelliJ
+ */
